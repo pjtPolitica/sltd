668300153ad6ed2fcbe18c443427ea17ea6ae757
pjtPolitica <pjt.politica@gmail.com>
2024-10-10 17:36:11 -0400

    configuração de arquivos para comunicar banco de dados

--- src/Config.php
+++ src/Config.php
@@ -6,9 +6,9 @@ class Config {
 
     const DB_DRIVER = 'mysql';
     const DB_HOST = 'localhost';
-    const DB_DATABASE = 'test';
+    const DB_DATABASE = 'soletrando';
     CONST DB_USER = 'root';
-    const DB_PASS = '';
+    const DB_PASS = 'palmira123';
 
     const ERROR_CONTROLLER = 'ErrorController';
     const DEFAULT_ACTION = 'index';
